1887564d0c825ca25518cedf7fda2b0d5737a9b5
Dmitriy L <dukat2007@gmail.com>
2023-08-18 18:02:39 +0700

    The basic autocorrection of the Sensor's internal memory

--- Sensor.php
+++ Sensor.php
@@ -3,10 +3,14 @@
 class Sensor
 {
 	protected const DEFAULT_ASSOCIATION = 0.1;
+	protected const CORRECTION_STEP = 0.1;
 
 	protected int $channelsNum;
 
 	/**
+	 * Насколько сильно сигнал по некоторому каналу
+	 * вызывает "ассоциацию" с некоторым результатом.
+	 *
 	 * [$channel => [string $possibleResult => float $association]]
 	 * 0 <= $association <= 1
 	 *
@@ -17,6 +21,8 @@ class Sensor
 	protected SplFixedArray $data;
 
 	protected SplFixedArray $possibleResults;
+
+	protected $valueNormalizer;
 
 	protected function initMemory(array $possibleResults): void
 	{
@@ -31,12 +37,106 @@ class Sensor
 		}
 	}
 
-	public function __construct(int $channelsNum, array $possibleResults)
+	protected function getAssociation(int $channel, string $possibleResult): float
+	{
+		return $this->memory[ $channel ][ $possibleResult ];
+	}
+
+	protected function setAssociation(int $channel, string $possibleResult, float $association): void
+	{
+		$memoryCell = $this->memory[ $channel ];
+		$memoryCell[ $possibleResult ] = $association;
+		$this->memory[ $channel ] = $memoryCell;
+	}
+
+	protected function increaseAssociation(int $channel, string $possibleResult): void
+	{
+		$association = $this->getAssociation($channel, $possibleResult);
+		$association *= (1 + self::CORRECTION_STEP);
+		if ($association > 1) {
+			$association = 1;
+		}
+		$this->setAssociation($channel, $possibleResult, $association);
+	}
+
+	protected function decreaseAssociation(int $channel, string $possibleResult): void
+	{
+		$association = $this->getAssociation($channel, $possibleResult);
+		$association *= (1 - self::CORRECTION_STEP);
+		$this->setAssociation($channel, $possibleResult, $association);
+	}
+
+	protected function randomChangeAssociation(int $channel, string $possibleResult): void
+	{
+		// TODO
+	}
+
+	public function __construct(int $channelsNum, array $possibleResults, callable $valueNormalizer)
 	{
 		$this->channelsNum = $channelsNum;
 		$this->initMemory($possibleResults);
-		$this->data = new SplFixedArray($channelsNum);
+		$this->data = new SplFixedArray($this->channelsNum);
 		$this->possibleResults = SplFixedArray::fromArray($possibleResults);
+		$this->valueNormalizer = $valueNormalizer;
+	}
+
+	/**
+	 * Отображение значение в интервал [0..1].
+	 *
+	 * @param  float  $value
+	 * @return float
+	 */
+	protected function normalizeValue(float $value): float
+	{
+		return call_user_func($this->valueNormalizer, $value);
+	}
+
+	public function dumpMemory(): SplFixedArray
+	{
+		return $this->memory;
+	}
+
+	public function putData(array $values): void
+	{
+		for ($channel = 0; $channel < $this->channelsNum; $channel++) {
+			$this->data[ $channel ] = isset($values[ $channel ])
+				? $this->normalizeValue($values[ $channel ])
+				: 0;
+		}
+	}
+
+	/**
+	 * Насколько уровень сигнала в каждом из каналов
+	 * напоминает _такой_ результат?
+	 *
+	 * @param  string  $possibleResult
+	 * @return float [0..1]
+	 */
+	protected function getAssociationLevel(string $possibleResult): float
+	{
+		$result = 0;
+		foreach ($this->data as $channel => $value) {
+			$result += $this->getAssociation($channel, $possibleResult) * $value;
+		}
+
+		// просто среднее значение? хм...
+		return ($result / $this->channelsNum);
+	}
+
+	/**
+	 * Вероятности того, что бы "увиден" каждый из возможных результатов.
+	 *
+	 * @return array
+	 */
+	public function getPrediction(): array
+	{
+		$result = [];
+
+		foreach ($this->possibleResults as $possibleResult) {
+			$result[ $possibleResult ] = $this->getAssociationLevel($possibleResult);
+		}
+
+		return $result;
 	}
 
 	/**
@@ -49,13 +149,30 @@ class Sensor
 	 */
 	protected function doCorrection(array $expectedResult): void
 	{
-		// TODO
 		foreach ($this->getPrediction() as $possibleResult => $association) {
-			// надо сравнить $association с $expectedResult[ $possibleResult ]
-			foreach ($this->data as $channel => $value) {
-				// если "вклад" ячейки памяти был положительным - усиливаем ассоциацию,
-				// иначе - уменьшаем
-				$this->memory[ $channel ][ $possibleResult ];
+			// надо сравнить $association с $expectedResult[ $possibleResult ];
+			// если "вклад" ячейки памяти был положительным - усиливаем ассоциацию,
+			// иначе - уменьшаем
+			if (
+				($expectedResult[ $possibleResult ] && $association > PREDICTION_THRESHOLD_TRUE)
+				|| (!$expectedResult[ $possibleResult ] && $association < PREDICTION_THRESHOLD_FALSE)
+			) {
+				// верный ответ, усиливаем ассоциацию
+				foreach ($this->data as $channel => $value) {
+					if ($value > PREDICTION_THRESHOLD_TRUE) {
+						$this->increaseAssociation($channel, $possibleResult);
+					} else {
+						$this->randomChangeAssociation($channel, $possibleResult);
+					}
+				}
+			} else {
+				foreach ($this->data as $channel => $value) {
+					if ($value > PREDICTION_THRESHOLD_TRUE) {
+						$this->decreaseAssociation($channel, $possibleResult);
+					} else {
+						$this->randomChangeAssociation($channel, $possibleResult);
+					}
+				}
 			}
 		}
 	}
@@ -64,51 +181,5 @@ class Sensor
 	{
 		$this->putData($values);
 		$this->doCorrection($expectedResult);
-	}
-
-	public function putData(array $values): void
-	{
-		$this->data = SplFixedArray::fromArray($values);
-		// TODO: проверка длины массива
-	}
-
-	protected function getMemory(int $channel, string $possibleResult): float
-	{
-		return $this->memory[ $channel ][ $possibleResult ];
-	}
-
-	/**
-	 * Насколько уровень сигнала в каждом из каналов
-	 * напоминает _такой_ результат?
-	 *
-	 * @param  string  $possibleResult
-	 * @return float [0..1]
-	 */
-	protected function getAssociation(string $possibleResult): float
-	{
-		// TODO: отображение в [0..1]
-		$result = 0;
-		foreach ($this->data as $channel => $value) {
-			$result += $this->getMemory($channel, $possibleResult) * $value;
-		}
-
-		return ($result / $this->channelsNum);
-	}
-
-	public function getPrediction(): array
-	{
-		$result = [];
-
-		foreach ($this->possibleResults as $possibleResult) {
-			$result[ $possibleResult ] = $this->getAssociation($possibleResult);
-		}
-
-		return $result;
-	}
-
-	public function getResult(): string
-	{
-		// TODO
-		return '';
 	}
 }
--- main.php
+++ main.php
@@ -1,10 +1,12 @@
 <?php
 
+define('PREDICTION_THRESHOLD_FALSE', 0.25);
+define('PREDICTION_THRESHOLD_TRUE', 0.75);
+
 require_once __DIR__ . '/ColorRGB.php';
 require_once __DIR__ . '/Pixel.php';
 require_once __DIR__ . '/DataGenerator.php';
 require_once __DIR__ . '/Sensor.php';
-require_once __DIR__ . '/Eye.php';
 
 function trainSensor(Sensor $Sensor, int $iterations = 10): void
 {
@@ -18,24 +20,34 @@ function trainSensor(Sensor $Sensor, int $iterations = 10): void
 }
 
 // тестирование обученной сети
-function testSensor(Sensor $Sensor, int $iterations = 10): void
+function testSensor(Sensor $Sensor, int $iterations = 10, bool $debug = false): void
 {
+	$passed = 0;
 	for ($i = 0; $i < $iterations; $i++) {
 		$Pixel = DataGenerator::getPixel();
 		$Sensor->putData( $Pixel->getRGB() );
 
-		$expected = $Pixel->getColor()->name;
-		$actual = $Sensor->getResult();
-		if ($expected === $actual) {
-			echo 'passed', PHP_EOL;
-		} else {
-			echo "expected: {$expected}, actual: {$actual}", PHP_EOL;
+		$realValue = $Pixel->getColor()->name;
+		$prediction = $Sensor->getPrediction();
+
+		if ($prediction[$realValue] > PREDICTION_THRESHOLD_TRUE) {
+			$passed++;
+		}
+
+		if ($debug) {
+			echo 'Real Value: ', $realValue, PHP_EOL;
+			var_dump($prediction);
 		}
 	}
+
+	echo "PASSED: {$passed}/{$iterations}", PHP_EOL;
 }
 
-//$Pixel = DataGenerator::getPixel();
-//echo $Pixel;
+$valueNormalizer = static function (float $value): float
+{
+	//return 1 / (1 + M_E ** -$value);
+	return $value / 255;
+};
 
 $Sensor = new Sensor(
 	3,
@@ -44,7 +56,9 @@ $Sensor = new Sensor(
 		ColorRGB::Green->name,
 		ColorRGB::Blue->name,
 		ColorRGB::Undefined->name
-	]
+	],
+	$valueNormalizer
 );
-trainSensor($Sensor);
+trainSensor($Sensor, 30);
+var_dump($Sensor->dumpMemory());
 //testSensor($Sensor);
